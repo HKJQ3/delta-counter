3a0e6939f0b4d9b6fa64b5dfc1d5a83089353284
HKJQ3 <2052346558@qq.com>
2025-10-03 00:56:02 +0800

    Update hit.js

--- api/hit.js
+++ api/hit.js
@@ -3,9 +3,9 @@ const redis = Redis.fromEnv()
 
 export default async (req, res) => {
   const user = req.query.user || 'unknown'
-  const key = `daily:${user}:${new Date().toISOString().slice(0,10)}` // 按天隔离
+  const key = `daily:${user}:${new Date().toISOString().slice(0,10)}`
   const n = await redis.incr(key)
-  await redis.expire(key, 86400)        // 24h 自动过期
+  await redis.expire(key, 86400)
   if (n > 10) return res.status(200).send('kick')
   res.status(200).send(String(n))
 }
